53272bfc30d722f6917c2f4d21c2eee8ef70f006
AI Deployer <deploy@ekaakshareducation.com>
2026-09-12 16:27:58 +0530

    feat(webhook): update Web App URL to latest deployment

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 /* --- CONFIGURATION --- */
 const AIPASSPORT_CONFIG = {
   // Connect to Google Sheets & Gmail Webhook
-  webhookUrl: "https://script.google.com/macros/s/AKfycbzDAbW8VFvsGyCHxhdYqio2IgsluSrBlkocFjOpnnUEg0cfpDlpF5n0soyIrX-ry1ZStQ/exec"
+  webhookUrl: "https://script.google.com/macros/s/AKfycbxabqaQAIYHpXv9I9wKIHK5Dii7u-VUjPxPWk1XHd0sySjzP-xA7MdHFgAP6nat0QtO1A/exec"
 };
 
 // Register GSAP plugins safely
